2c7a162c213f2c6e67067a7d9d3711405e1b1de3
HE1BAI <haobowan@buffalo.edu>
2023-10-04 20:23:02 -0400

    hash password

--- nutrients-fanclub/backend/login.php
+++ nutrients-fanclub/backend/login.php
@@ -1,53 +1,35 @@
 <?php
-include "config.php";
-
-// Start the session
+include "db.php";
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    // Protect against SQL injection
-    $username = mysqli_real_escape_string($conn, $username);
-
-    $query = "SELECT id, password FROM users WHERE username = '$username'";
-    $result = $conn->query($query);
-
-    if ($result->num_rows > 0) {
-        // Fetching the hashed password from the database
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+    if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
-        if (password_verify($password, $row["password"])) {
-            // Password is correct, set the session
-            $_SESSION["loggedin"] = true;
-            $_SESSION["id"] = $row["id"];
-            $_SESSION["username"] = $username;
-
-            header("location: welcome.php");
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+            echo "Logged in successfully!";
         } else {
-            $error = "Your Login Name or Password is invalid";
+            echo "Invalid password!";
         }
     } else {
-        $error = "Your Login Name or Password is invalid";
+        echo "Invalid username!";
     }
+    
+    $stmt->close();
 }
 ?>
 
-<!-- HTML for the login form -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-
-<form action="" method="post">
-    <label>Username :</label>
-    <input type="text" name="username"><br><br>
-    <label>Password :</label>
-    <input type="password" name="password"><br><br>
-    <input type="submit" value=" Submit "><br>
+<form method="post" action="">
+    Username: <input type="text" name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <input type="submit" value="Login">
 </form>
-
-</body>
-</html>
