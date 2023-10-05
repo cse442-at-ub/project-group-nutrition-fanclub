b901fd7c2992702aad6ef9ecb92851426a78b5c9
rzhang25 <ruifengest@gmail.com>
2023-10-05 14:41:54 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleDarkMode = () => setDarkMode(!darkMode);
+
+    return (
+        <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+            <header style={{ backgroundColor: '#2E6F57' }}>
+                <span>UGRUB</span>
+                <div class="button-container">
+                    <label class="darkmode-label">Dark
+                    <div className="toggle" onClick={toggleDarkMode}></div>
+                    </label>    
+                    <button class="home-button">HOME</button>
+                    <button class="login-button">LOG IN</button>
+                </div>
+            </header>
+            <main>
+                <input placeholder="Usename/Email address" />
+                <input type="password" placeholder="Password" />
+                <button className="login-btn">LOGIN</button>
+                <div>
+                    <span>Not registered? </span><a href="#">Create an account</a>
+                </div>
+                <div>
+                    <a href="#">Forget Password?</a>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
