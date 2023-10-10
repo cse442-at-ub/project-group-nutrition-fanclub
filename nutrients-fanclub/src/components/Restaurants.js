c5ab8e6792ef803ab042336f6f9e6bc3c0305bdb
haoqinzh <haoqinzh@buffalo.edu>
2023-10-10 15:46:53 -0400

    Fixed homepage

--- nutrients-fanclub/src/components/Restaurants.js
+++ nutrients-fanclub/src/components/Restaurants.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Restaurants() {
   return (
-    <div className="row row-cols-7 row-cols-md-7 g-4" style={{ padding: '2%', paddingLeft: 180}}>
+    <div className="row row-cols-7 row-cols-md-7 g-4" style={{ padding: '2%', paddingLeft: 160}}>
       <div className="col">
         <div className="card">
           <img src="https://lh3.googleusercontent.com/p/AF1QipP5GR5miR3USPUEfvjO2tafEjSPd9DXPv8pFx3N=s1360-w1360-h1020" className="card-img-top" alt="..." style={{ maxHeight: 'fit-content' }} />
--- nutrients-fanclub/src/components/Sidebar.js
+++ nutrients-fanclub/src/components/Sidebar.js
@@ -1,15 +1,62 @@
-// Sidebar.js
 import React from 'react';
 
 function Sidebar() {
   return (
-    <div class="sidebar">
-      <ul>
-        <li>Dashboard</li>
-        <li>Profile</li>
-        <li>Messages</li>
-        <li>Settings</li>
-      </ul>
+    <div className="container">
+      <div className="sidebar">
+        <ul>
+          <li style={{paddingTop: '20px'}}></li>
+          <li><h4><a>Filter by</a></h4></li>
+          <li style={{paddingTop: '1px'}}></li>
+          <li><h5><a>Cuisine</a></h5></li>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck"></input>
+            <label class="form-check-label" for="DefaultCheck">American</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck2"></input>
+            <label class="form-check-label" for="DefaultCheck2">Chinese</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck3"></input>
+            <label class="form-check-label" for="DefaultCheck3">Halal</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck4"></input>
+            <label class="form-check-label" for="DefaultCheck4">Indian</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck5"></input>
+            <label class="form-check-label" for="DefaultCheck5">Japanese</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck6"></input>
+            <label class="form-check-label" for="DefaultCheck6">Korean</label>
+          </div>
+          <hr></hr>
+          <li><h5><a>Food Options</a></h5></li>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck7"></input>
+            <label class="form-check-label" for="DefaultCheck7">Boba</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck8"></input>
+            <label class="form-check-label" for="DefaultCheck8">Breakfast</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck9"></input>
+            <label class="form-check-label" for="DefaultCheck9">Pizza</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck10"></input>
+            <label class="form-check-label" for="DefaultCheck10">Platter</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="DefaultCheck11"></input>
+            <label class="form-check-label" for="DefaultCheck11">Noodles</label>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 }
