bcf0dcb12e3a1d4461316d077daac4d73f55cf36
Ryan Khan <ryankhan@buffalo.edu>
2023-10-16 14:21:00 -0400

    signup ver 1

--- nutrients-fanclub/public/signup_backend.php
+++ nutrients-fanclub/public/signup_backend.php
@@ -1,45 +1,73 @@
-user_details = []
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-function validateUsername($username) {
-    // Username should be between 3 and 20 characters
-    $min_length = 3;
-    $max_length = 20;
-    
-    if (strlen($username) < $min_length || strlen($username) > $max_length) {
-        return false;
+// Function to establish a database connection
+function connectToDatabase() {   
+    $host = "oceanus.cse.buffalo.edu"; // MySQL server hostname
+    $username = "ryankhan"; // MySQL username
+    $password = "50389982"; // MySQL password
+    $database = "cse442_2023_fall_team_ae_db"; // Name of your MySQL database
+
+    $mysqli = new mysqli($host, $username, $password, $database);
+
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
     }
-    
-    // Additional validation rules can be added here
-    // For example, you might want to check for alphanumeric characters only
-    
-    return true;
+
+    return $mysqli;
 }
 
-function validatePassword($password) {
-    // Password should be at least 8 characters long
-    $min_length = 8;
-    
-    if (strlen($password) < $min_length) {
-        return false;
+// Function to create the 'users' table
+function createUsersTable() {
+    $mysqli = connectToDatabase();
+    // SQL statement to create the 'users' table
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        favorite_restaurant VARCHAR(255) NOT NULL
+    )";
+
+    if ($mysqli->query($sql) === TRUE) {
+        echo "Table 'users' created successfully!";
+    } else {
+        echo "Error creating table: " . $mysqli->error;
     }
-    
-    // Additional validation rules can be added here
-    // For example, you might want to check for uppercase, lowercase, and numbers
-    
-    return true;
-}
-function favrestuarant($restaurant){
-    
+
+    $mysqli->close();
 }
 
-function 
-function hashPassword($password) {
-    // Use a strong hashing algorithm like bcrypt
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    return $hashed_password;
+// Function to add a new user to the database
+function addUser($username, $password, $email, $favoriteRestaurant) {
+    $mysqli = connectToDatabase();
+
+    // Hash the password for security
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+    // Prepare the SQL statement
+    $stmt = $mysqli->prepare("INSERT INTO users (username, password, email, favorite_restaurant) VALUES (?, ?, ?, ?)");
+
+    // Bind parameters
+    $stmt->bind_param("ssss", $username, $hashedPassword, $email, $favoriteRestaurant);
+
+    // Execute the statement
+    if ($stmt->execute()) {
+        echo "User registration successful!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the statement and database connection
+    $stmt->close();
+    $mysqli->close();
 }
 
-function verifyPassword($password, $hashed_password) {
-    // Verify a password against its hashed version
-    return password_verify($password, $hashed_password);
-}
+// Call the createUsersTable function to create the 'users' table
+createUsersTable();
+
+// Example usage to add a new user
+addUser("Mr.Hotdog", "buzzlightyear", "mrhotdog@example.com", "Wendy's");
+addUser("meowman", "rtcgvfhbjnksc", "meowman@example.com", "arbys");
+addUser("hao", "69420550", "hao@example.com", "Young chow");
+?>
