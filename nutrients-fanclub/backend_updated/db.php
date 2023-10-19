76ab67384b4ec92bd49e89b98b5198e7ee58cc01
Ryan Khan <ryankhan@buffalo.edu>
2023-10-19 16:07:48 -0400

    signup_validate deleted, vlidation functions are now added to signup_storeinDB

---
+++ nutrients-fanclub/backend_updated/db.php
@@ -0,0 +1,20 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+//this script is for db connection, the signup_storeinDB.php file doesn't import this because i wrote the databse connection in there anyway- ryan
+// Function to establish a database connection
+function connectToDatabase() {   
+    $host = "oceanus.cse.buffalo.edu"; // 
+    $username = "haobowan"; // 
+    $password = "50375823"; // 
+    $database = "cse442_2023_fall_team_ae_db"; // 
+
+    $mysqli = new mysqli($host, $username, $password, $database);
+
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    return $mysqli;
+}
+
+?>
