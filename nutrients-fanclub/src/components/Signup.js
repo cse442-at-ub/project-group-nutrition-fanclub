9634241850cfaf7b57ab86b037611aabec6de1ef
Kokoxili <90289330+Kokoxili@users.noreply.github.com>
2023-10-21 22:23:03 -0400

    final update

--- nutrients-fanclub/src/components/Signup.js
+++ nutrients-fanclub/src/components/Signup.js
@@ -1,18 +1,24 @@
 import Axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Bigbutton from './Bigbutton';
 import Content_signup from "./Content_signup";
 import Input_login from './Input_login';
+
+import './style.css';
 
 function Signup() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [favoriteRestaurant, setFavoriteRestaurant] = useState("");
+    const [errors, setErrors] = useState({});
+    const [genericError, setGenericError] = useState('');
+
+    const navigate = useNavigate('/CSE442-542/2023-Fall/cse-442ae/build/login');
 
     const signUpUser = (event) => {
-        event.preventDefault();  // This will prevent the default action of the Link
+        event.preventDefault();
 
         const userData = {
             username: username,
@@ -21,36 +27,41 @@ function Signup() {
             favoriteRestaurant: favoriteRestaurant
         };
 
-        // Store the user information
-        Axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ae/backend_updadated_new/signupv3.php', userData)
+        Axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ae/backend/signupFinal.php', userData)
             .then(response => {
-                if (response.data.status == 1) {
+                if (response.data.status === 1) {
                     console.log("send success");
-                    // Handle successful registration. For instance, you might navigate the user to the login page or show a success message.
-                } else if (response.data.status == 0) {
+                    setErrors({}); // Clear any errors
+                } else if (response.data.status === 0) {
                     console.log("Error:", response.data.message);
-                    console.log("Validation Issues:", response.data.errors);
-                    // Handle the errors, for instance, show an error message to the user.
+                    setErrors(response.data.errors);
                 }
             })
             .catch(error => {
-                console.log(error);
-                // Handle network errors or unexpected issues.
+                console.log(error, 'catch the hoop');
+                setGenericError('Something went wrong. Please try again.');
             });
     };
 
     return (
         <div>
             <div className='app-container' style={{ paddingTop: 150 }}>
-                <span>
-                    <Input_login placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <Input_login placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <Input_login placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                {genericError && <div className="error-message">{genericError}</div>}
+                <form onSubmit={signUpUser}>
+                    <Input_login placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    {errors.username && <div className="error-message">{errors.username}</div>}
+
+                    <Input_login placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    {errors.email && <div className="error-message">{errors.email}</div>}
+
+                    <Input_login placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {errors.password && <div className="error-message">{errors.password}</div>}
+
                     <Input_login placeholder="Favorite Restaurant" value={favoriteRestaurant} onChange={(e) => setFavoriteRestaurant(e.target.value)} />
-                </span>
-                <Link to='/CSE442-542/2023-Fall/cse-442ae/build/login' onClick={signUpUser}>
+                    {errors.favoriteRestaurant && <div className="error-message">{errors.favoriteRestaurant}</div>}
+
                     <Bigbutton text="CREATE" />
-                </Link>
+                </form>
                 <Content_signup />
             </div>
         </div>
