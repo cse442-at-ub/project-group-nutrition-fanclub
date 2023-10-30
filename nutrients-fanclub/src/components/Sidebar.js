cac80f0e88219817a28f120fc8a9988d14d77f2b
haoqinzh <haoqinzh@buffalo.edu>
2023-10-30 17:56:38 -0400

    added post requests

--- nutrients-fanclub/src/components/Sidebar.js
+++ nutrients-fanclub/src/components/Sidebar.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Sidebar() {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  // Function to handle filter selection
+  const handleFilterSelection = (filter) => {
+    const updatedFilters = [...selectedFilters];
+
+    if (updatedFilters.includes(filter)) {
+      updatedFilters.splice(updatedFilters.indexOf(filter), 1);
+    } else {
+      updatedFilters.push(filter);
+    }
+    setSelectedFilters(updatedFilters);
+  };
+
+  // Function to send selected filters to the server
+  const sendFiltersToServer = () => {
+    Axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ae/backend_signup/filters.php', { filters: selectedFilters })
+      .then((response) => {
+        // Handle the response from the server
+        setFilteredRestaurants(response.data); // You can update your UI based on the response
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  useEffect(() => {
+    sendFiltersToServer();
+  }, [selectedFilters]);
+
   return (
     <div className="container">
       <div className="sidebar">
@@ -11,27 +43,69 @@ function Sidebar() {
             <hr></hr>
             <li><h5><a>Cuisine</a></h5></li>
             <div class="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="American"
+                id="DefaultCheck"
+                onChange={() => handleFilterSelection("American")}
+                checked={selectedFilters.includes("American")}
+              />
               <label className="form-check-label" for="DefaultCheck">American</label>
             </div>
             <div class="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck2"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Chinese"
+                id="DefaultCheck2"
+                onChange={() => handleFilterSelection("Chinese")}
+                checked={selectedFilters.includes("Chinese")}
+              />
               <label className="form-check-label" for="DefaultCheck2">Chinese</label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck3"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Halal"
+                id="DefaultCheck3"
+                onChange={() => handleFilterSelection("Halal")}
+                checked={selectedFilters.includes("Halal")}
+              />
               <label className="form-check-label" for="DefaultCheck3">Halal</label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck4"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Indian"
+                id="DefaultCheck4"
+                onChange={() => handleFilterSelection("Indian")}
+                checked={selectedFilters.includes("Indian")}
+              />
               <label className="form-check-label" for="DefaultCheck4">Indian</label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck5"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Japanese"
+                id="DefaultCheck5"
+                onChange={() => handleFilterSelection("Japanese")}
+                checked={selectedFilters.includes("Japanese")}
+              />
               <label className="form-check-label" for="DefaultCheck5">Japanese</label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="DefaultCheck6"></input>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="Korean"
+                id="DefaultCheck6"
+                onChange={() => handleFilterSelection("Korean")}
+                checked={selectedFilters.includes("Korean")}
+              />
               <label className="form-check-label" for="DefaultCheck6">Korean</label>
             </div>
             <hr></hr>
