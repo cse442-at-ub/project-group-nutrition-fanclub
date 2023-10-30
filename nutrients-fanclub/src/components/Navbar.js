c853b177969f9f61745ee7165f535432037460b7
Kokoxili <90289330+Kokoxili@users.noreply.github.com>
2023-10-30 15:34:38 -0400

    darkmode

--- nutrients-fanclub/src/components/Navbar.js
+++ nutrients-fanclub/src/components/Navbar.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-  function Navbar({isLogin, handleLogout}) {
-    return (
+function Navbar({isLogin, handleLogout}) {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Check for dark mode preference in local storage on component mount
+    const darkModePreference = localStorage.getItem('dark-mode');
+    if (darkModePreference === 'enabled') {
+      setIsDarkMode(true);
+      document.body.classList.add('dark-mode');
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+
+    if (document.body.classList.contains('dark-mode')) {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('dark-mode', 'disabled');
+    } else {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('dark-mode', 'enabled');
+    }
+  };
+
+  return (
       <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#2E6F57' }}>
         <div className="container-fluid">
           <span className="navbar-brand" id="title"><h1>UGRUB</h1></span>
           <li className="form-check form-switch ms-auto">
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault"
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+            />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault"><a>DARK</a></label>
           </li>
           <a style={{ padding: 7 }}></a>
@@ -21,11 +52,11 @@ import './style.css';
               <Link to="/CSE442-542/2023-Fall/cse-442ae/build/login">
                 <button className="btn btn-light rounded-pill" type="button" style={{ color: "#2E6F57" }}>SIGN IN</button>
               </Link>
-            </ul> 
+            </ul>
           </div>
         </div>
       </nav>
-    );
-  }
+  );
+}
 
-  export default Navbar;
+export default Navbar;
