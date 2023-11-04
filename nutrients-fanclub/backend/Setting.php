3bc76a74d569c89fecd9765c85f11414ca73adf1
HE1BAI <haobowan@buffalo.edu>
2023-11-04 15:57:05 -0400

    Update Setting.php

--- nutrients-fanclub/backend/Setting.php
+++ nutrients-fanclub/backend/Setting.php
@@ -1,5 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
 
