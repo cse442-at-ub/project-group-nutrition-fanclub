51d9ab20b047a9881404f42113500ec873e080fd
HE1BAI <haobowan@buffalo.edu>
2023-11-04 21:55:20 -0400

    update Email

---
+++ nutrients-fanclub/backend/SentEmail.php
@@ -0,0 +1,48 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Manually load PHPMailer class files
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+// Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->SMTPDebug = 2;                                    // Enable verbose debug output
+    $mail->isSMTP();                                          // Send using SMTP
+    $mail->Host       = 'smtp.example.com';                   // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                 // Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                   // SMTP username
+    $mail->Password   = 'secret';                             // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          // Enable implicit TLS encryption
+    $mail->Port       = 465;                                  // TCP port to connect to
+
+    // Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');         // Add a recipient
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');             // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');        // Optional name
+
+    // Content
+    $mail->isHTML(true);                                      // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
---
+++ nutrients-fanclub/backend/send.php
@@ -0,0 +1,40 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Manually load PHPMailer class files
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'heibaisedemao@gmail.com'; // Your gmail address
+    $mail->Password = 'xolywhbrlzxbdfdl'; // Your gmail app password
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('heibaisedemao@gmail.com'); // Your email
+    $mail->addAddress($_POST["email"]);
+    
+
+$mail->isHTML(true);
+
+$mail->Subject = $_POST["subject"];
+$mail->Body = $_POST["message"];
+
+$mail->send();
+echo"<script>
+alert('Sent Successfully');
+document.location.href = 'email.php';
+</script>";
+
+}
+
+?>
