943417640fbecdec680f654674dc450ec18ac56b
HE1BAI <haobowan@buffalo.edu>
2023-11-04 22:38:35 -0400

    update Email

--- nutrients-fanclub/backend/SentEmail.php
+++ nutrients-fanclub/backend/SentEmail.php
@@ -16,28 +16,26 @@ try {
     // Server settings
     $mail->SMTPDebug = 2;                                    // Enable verbose debug output
     $mail->isSMTP();                                          // Send using SMTP
-    $mail->Host       = 'smtp.example.com';                   // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                   // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                 // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                   // SMTP username
-    $mail->Password   = 'secret';                             // SMTP password
+    $mail->Username   = 'heibaisedemao@gmail.com';                   // SMTP username
+    $mail->Password   = 'xolywhbrlzxbdfdl';                             // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          // Enable implicit TLS encryption
     $mail->Port       = 465;                                  // TCP port to connect to
 
-    // Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');         // Add a recipient
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+
+    $mail->setFrom('heibaisedemao@gmail.com','Sender'); // Your email
+    $mail->addAddress('heibaisedemao@gmail.com','user'); // email should in the db
+   
 
     // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');             // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');        // Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz');             // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');        // Optional name
 
     // Content
     $mail->isHTML(true);                                      // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'This is the verification code';
+    $mail->Body    = 'This is your verification code <b>xxxxxxx!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
