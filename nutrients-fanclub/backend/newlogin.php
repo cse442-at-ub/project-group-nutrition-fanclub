6b26206ae961df7fe0a82620f1aa3f0abf79915d
HE1BAI <haobowan@buffalo.edu>
2023-11-05 13:45:05 -0500

    fix error

--- nutrients-fanclub/backend/newlogin.php
+++ nutrients-fanclub/backend/newlogin.php
@@ -1,7 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
 header("Content-Type: application/json");
+
+// If an OPTIONS request is received, respond with a 200 status code without any further action.
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include 'db.php';
 
