ed59202091241d52a14a7a84734e772fb637a024
HE1BAI <haobowan@buffalo.edu>
2023-11-19 20:44:59 -0500

    fix bug
    The problem of different front-end and back-end parameters

--- nutrients-fanclub/backend/Database_for_restaurant.php
+++ nutrients-fanclub/backend/Database_for_restaurant.php
@@ -5,50 +5,25 @@ include 'db.php';
 
 $mysqli = connectToDatabase();
 
-$createRestaurantsTable = "CREATE TABLE IF NOT EXISTS restaurants_InFo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    location TEXT NOT NULL
-)";
 
-// try SQL 
-if ($mysqli->query($createRestaurantsTable) === TRUE) {
-    echo "Table restaurants_InFo created successfully";
+$dropForeignKey = "ALTER TABLE User_Comments DROP FOREIGN KEY User_Comments_ibfk_1;";
+
+if ($mysqli->query($dropForeignKey) === TRUE) {
+    echo "Foreign key constraint dropped successfully";
 } else {
-    echo "Error creating table: " . $mysqli->error;
+    echo "Error dropping foreign key constraint: " . $mysqli->error;
 }
 
-// SQL create User_Comments table
-$createReviewsTable = "CREATE TABLE IF NOT EXISTS User_Comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    restaurant_id INT,
-    user_id INT,
-    comment TEXT,
-    rating DECIMAL(2,1),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants_InFo(id)
-    ON DELETE CASCADE
-)";
 
-// try SQL 
-if ($mysqli->query($createReviewsTable) === TRUE) {
-    echo "Table reviews created successfully";
-} else {
-    echo "Error creating table: " . $mysqli->error;
-}
 
-$modifyCommentsTable = "ALTER TABLE User_Comments CHANGE user_id username VARCHAR(255) NOT NULL;";
+$modifyCommentsTable = "ALTER TABLE User_Comments CHANGE restaurant_id restaurant_name VARCHAR(255) NOT NULL;";
 
-// SQL
 if ($mysqli->query($modifyCommentsTable) === TRUE) {
     echo "Table User_Comments modified successfully";
 } else {
     echo "Error modifying table: " . $mysqli->error;
 }
 
-
-
-$mysqli->close();
 function insertRestaurant($name, $location) {
     // 
     global $mysqli; 
--- nutrients-fanclub/backend/Save_Comments.php
+++ nutrients-fanclub/backend/Save_Comments.php
@@ -1,37 +1,53 @@
 <?php
-
+// Set headers for CORS and content type.
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
 
+// Include the database connection file.
 include 'db.php';
 
+// Connect to the database using a function from your 'db.php' file.
 $mysqli = connectToDatabase();
 
-// 
+// Check if the database connection was successful.
+if (!$mysqli) {
+    // Log the error message and return a JSON response if the connection failed.
+    error_log("Database connection error: " . $mysqli->connect_error);
+    echo json_encode(array('message' => 'Database connection error', 'status' => false));
+    exit; // Exit the script after sending the response.
+}
+
+// Read the JSON input from the request.
 $jsonData = file_get_contents('php://input');
 error_log("Received data: " . $jsonData);
 
+// Decode the JSON input into a PHP object.
 $data = json_decode($jsonData);
 
-// 
+// Check if there was a JSON decode error and respond accordingly.
 if (json_last_error() !== JSON_ERROR_NONE) {
     error_log("JSON decode error: " . json_last_error_msg());
     echo json_encode(array('message' => 'JSON decode error: ' . json_last_error_msg(), 'status' => false));
-    exit;
+    exit; // Exit the script after sending the response.
 }
 
-$stmt = $mysqli->prepare("INSERT INTO User_Comments (restaurant_id, username, comment, rating) VALUES (?, ?, ?, ?)");
+// Prepare the SQL statement for inserting data into the 'User_Comments' table.
+$stmt = $mysqli->prepare("INSERT INTO User_Comments (restaurant_name, username, comment, rating) VALUES (?, ?, ?, ?)");
 
+// Check if the prepared statement was successful.
 if (!$stmt) {
     error_log("Prepare statement error: " . $mysqli->error);
     echo json_encode(array('message' => 'Prepare statement error: ' . $mysqli->error, 'status' => false));
-    exit;
+    exit; // Exit the script after sending the response.
 }
 
-if (isset($data->restaurant_id, $data->username, $data->comment, $data->rating)) {
-    $stmt->bind_param("isss", $data->restaurant_id, $data->username, $data->comment, $data->rating);
+// Check if the necessary data is set in the received JSON.
+if (isset($data->restaurant_name, $data->username, $data->comment, $data->rating)) {
+    // Bind the parameters to the prepared statement.
+    $stmt->bind_param("sssd", $data->restaurant_name, $data->username, $data->comment, $data->rating);
     
+    // Attempt to execute the prepared statement and respond accordingly.
     if ($stmt->execute()) {
         echo json_encode(array('message' => 'Comment added successfully', 'status' => true));
     } else {
@@ -43,7 +59,10 @@ if (isset($data->restaurant_id, $data->username, $data->comment, $data->rating))
     echo json_encode(array('message' => 'Missing data', 'status' => false));
 }
 
+// Close the prepared statement.
 $stmt->close();
+
+// Close the database connection.
 $mysqli->close();
 
 ?>
