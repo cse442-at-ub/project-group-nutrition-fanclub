7d4c8e8e6f8aac1c6ec7e8c2cf46ff3487806763
haoqinzh <haoqinzh@buffalo.edu>
2023-11-20 00:51:50 -0500

    post

--- nutrients-fanclub/src/components/Layout.js
+++ nutrients-fanclub/src/components/Layout.js
@@ -1,33 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Layout.css';
+import axios from "axios";
 
 function Layout(){
+    const [comment, setComment] = useState('');
+    const [rating, setRating] = useState('');
+    const userEmail = localStorage.getItem('useremail');
     const RD = {
-        name: "Sample Name",
+        restaurant_name: "Sample Name",
         address: "Sample address",
         count: 68,
-        rate: "3.0",
-        tags: ["tag1","tag2","tag3","tag4","tag5"],
+        average_rating: "3.0",
         lastPerson: "Sample User",
         lastRating: "2.0",
-        lastcontent: "Overall the experience  I had at c3 was pretty terrible. I saw a rat inside the matzo ball soup and every single hand-sanitizer dispenser had nothing left. I did like the ice cream however",
-        image: ["image1", "image2","image3"]
+        lastcontent: "",
+        image: ["image1", "image2", "image3"]
     };
     const [restaurantData, setRestaurantData] = useState(RD);
 
+    const sendReview = () => {
+        if (!comment || !rating) {
+            console.error('Comment and rating are required');
+            return;
+        }
+
+        const data = {
+            restaurant_id: restaurantData.restaurant_name,
+            user_id: userEmail,
+            comment: comment,
+            rating: rating,
+        };
+
+        axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ae/backend/reviews.php', data)
+            .then((response) => {
+                console.log('Review submitted successfully:', response.data);
+            })
+            .catch((error) => {
+                console.error('Error submitting review:', error);
+            });
+    };
+
+    useEffect(() => {
+        // Fetch restaurant data from the server
+        axios.get('https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442ae/backend/reviews.php')
+            .then((response) => {
+                const data = response.data;
+
+                if (data.status === 1) {
+                    setRestaurantData({
+                        restaurant_name: data.restaurantID,
+                        address: data.address,
+                        count: data.count,
+                        average_rating: data.average_rating,
+                        tags: data.tags,
+                        lastPerson: data.lastPerson,
+                        lastRating: data.lastRating,
+                        lastcontent: data.lastcontent,
+                        image: data.image,
+                    });
+                } else {
+                    console.error('Failed to fetch restaurant data:', data.message);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching restaurant data:', error);
+            });
+    }, []); // Empty dependency array means this effect runs once after the initial render
 
     return(
         <div className="restaurant-container">
-            <h1>{restaurantData.name}</h1>
+            <h1>{restaurantData.restaurant_name}</h1>
             <p>{restaurantData.address}</p>
             <div className="rating">
-                <span>⭐️ {restaurantData.rate}</span>
+                <span>⭐️ {restaurantData.average_rating}</span>
                 <span>({restaurantData.count} reviews)</span>
-            </div>
-            <div className="tags">
-                {restaurantData.tags.map(tag => (
-                <span key={tag} className="tag">{tag}</span>
-                ))}
             </div>
                 <div className="reviews">
                 <div className="user">{restaurantData.lastPerson}</div>
@@ -38,6 +84,28 @@ function Layout(){
                 {restaurantData.image.map((img, index) => (
                 <img key={index} src={img} alt={`restaurant-img-${index}`} />
                 ))}
+            </div>
+            <div className="comment">
+                <label for="comment_box">Comment: 
+                    <input
+                        id="comment_box"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+                </label>
+                <span style={{padding:3}}/>
+                <label for="rating_box">Rating:
+                    <input
+                        type="number"
+                        min="0"
+                        max="5"
+                        id="rating_box"
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                    />
+                </label>
+                <span style={{padding:3}}/>
+                <button className="btn btn-primary" onClick={sendReview}>Submit</button>
             </div>
         </div>
     );
